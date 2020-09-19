2ece21ccd8f7e8012db1b833ae126f76bbfc5dde
Eric Lawrence <eric@squaretone.com>
2020-09-19 15:24:40 -0500

    Base react app

---
+++ src/js/AppContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from './App'
+
+const AppContext = () => {
+  return (
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  )
+}
+
+export default AppContext
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppContext from './AppContext'
+
+ReactDOM.render(
+  <AppContext />,
+  document.querySelector('#root')
+)
